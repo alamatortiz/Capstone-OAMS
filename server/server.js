4d6192a2af055b6bda581df98089de5173657061
Joronski <joronrecio@yahoo.com>
2026-09-02 12:41:25 +0800

    Fix: Render Deployment Currently Not Working

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = (
   process.env.CLIENT_ORIGINS ||
-  "http://localhost:5173,https://capstone-coams-pnc.onrender.com"
+  "https://capstone-coams-pnc.onrender.com"
 )
   .split(",")
   .map((o) => o.trim());
